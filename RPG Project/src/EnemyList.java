470b56029af7f5ee8947b1089adba4b9eb7bcd85
nicholas.sauro <nicholas.sauro@MU-C1-14.mullen.pvt.k12.co.us>
2021-11-09 11:35:41 -0700

    finished project :)

--- RPG Project/src/EnemyList.java
+++ RPG Project/src/EnemyList.java
@@ -9,7 +9,7 @@ public class EnemyList
 			{
 						
 				enemies.add(new Enemy("rat", "teeth", "disease", 20, 12, 60.0));
-				enemies.add(new Enemy("slime", "ooze", "absolute annihilation", 40, 1, 75.5));
+				enemies.add(new Enemy("slime", "ooze", "absolute annihilation", 40, 50, 75.5));
 				enemies.add(new Enemy("zombie", "bite", "enrage", 35, 70, 55.0));
 				enemies.add(new Enemy("dragon", "fire breath", "slaughter", 150, 90, 90.9));
 				
--- RPG Project/src/mainRPG.java
+++ RPG Project/src/mainRPG.java
@@ -26,6 +26,7 @@ public class mainRPG
 		public static void main(String[] args)
 			{
 				EnemyList.fillArray();
+				PlayerAL.playerAF();
 				welcomePlayer();
 				enemySelect();
 				secstop();
@@ -54,6 +55,7 @@ public class mainRPG
 			halfsecstop();
 			System.out.println("[BATTLE START]");
 		}
+		
 		public static void clearConsole()
 		{
 			for(int i = 0; i<3; i++) 
@@ -61,26 +63,67 @@ public class mainRPG
 				System.out.println("					");
 			}
 		}
-		public static void battle()
+		public static void menu()
 		{
-			Scanner userStringInput = new Scanner(System.in);
 			System.out.println("You are fighting the " + EnemyList.enemies.get(number).getName() + "." );
 			System.out.println("--------");
 			System.out.println("ATTACK");
 			System.out.println("BLOCK");
 			System.out.println("CHECK");
 			System.out.println("ITEM");
+		}
+		public static void enemyTurn()
+		{
+			int enemyChoice = (int)(Math.random()*2);
+			if (enemyChoice == 1)
+				{
+					//if(block==false)
+						{
+					PlayerAL.playerset.get(0).setHealth(PlayerAL.playerset.get(0).getHealth() - EnemyList.enemies.get(number).getDamage());
+					System.out.println(EnemyList.enemies.get(number).getName()+ " uses " + EnemyList.enemies.get(number).getAttack() 
+							+ "! Your health is reduced to " + PlayerAL.playerset.get(0).getHealth() + ".");
+				}
+		}
+		}
+		public static void battle()
+		{
+			Scanner userStringInput = new Scanner(System.in);
+			menu();
 			String menu = userStringInput.nextLine();
 			if(menu.equals("attack"))
 				{
-					
+					int hitprob = (int)(Math.random()*100) + 1;
+					if(hitprob < PlayerAL.playerset.get(0).getHitprob())
+						{
+						EnemyList.enemies.get(number).setHealth(EnemyList.enemies.get(number).getHealth() - PlayerAL.playerset.get(0).getAttack());
+						System.out.println("You attack the " + EnemyList.enemies.get(number).getName() + ", dealing " + PlayerAL.playerset.get(0).getAttack() + " damage. The "
+						+ EnemyList.enemies.get(number).getName() + "'s health is now " + EnemyList.enemies.get(number).getHealth() + "!" );
+						}else
+							{
+								System.out.println("You missed!");
+								enemyTurn();
+							}
+					if(EnemyList.enemies.get(number).getHealth() <= 0)
+						{
+							System.out.println("You win!");
+						}
+					enemyTurn();
 				} else if(menu.equals("block")) 
 					{
-					
+						Boolean block = true;
+						System.out.println("You brace for impact!");
+						enemyTurn();
 				} else if(menu.equals("check"))
 					{
 						System.out.println( EnemyList.enemies.get(number).getName() + ", " + "ATK " + EnemyList.enemies.get(number).getDamage());
-					}
+					} else if(menu.equals("item"))
+						{
+							
+						}else 
+							{
+							System.out.println("Please select one of the options below");
+							menu();
+						}
 			
 		}
 	
